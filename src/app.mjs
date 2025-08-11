873a0aa72df57d2cf0738806de2f3e6c4cec0026
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2025-08-11 17:17:49 +0100

    Implement app as per tutorial

--- src/app.mjs
+++ src/app.mjs
@@ -1,1 +1,12 @@
-console.log('WOWSER!');
+import indexRouter from './routers/indexRouter.mjs';
+
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.use('/', indexRouter);
+
+app.listen(process.env.PORT, () =>
+  console.log('Server listening on port', process.env.PORT),
+);
